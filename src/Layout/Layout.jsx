53fd2c54c95ef31ab9997ebd8da4a8be0e2e8db7
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-02 15:08:01 +0600

    creating navbar section

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
 
 
 const Layout = () => {
   return (
-    <div>
+    <div className="bg-[#2D3639]">
+      {/* navbar section start */}
+      <Navbar></Navbar>
+      {/* navbar section end */}
       {/* content section start */}
       <Outlet></Outlet>
       {/* content section end */}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { RiBarChartHorizontalFill } from "react-icons/ri";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="h-28 py-5">
+      {/* content section start */}
+      <div className="container mx-auto  w-full px-5 lg:px-0 space-y-5">
+        {/* profile and logo section start */}
+        <div className="flex justify-between items-center h-full w-full">
+          {/* logo section start */}
+          <RiBarChartHorizontalFill className="text-gray-200 text-2xl"></RiBarChartHorizontalFill>
+          {/* logo section end */}
+          {/* profile section start */}
+          <div className="avatar online">
+            <div className="w-11 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          {/* profile section end */}
+        </div>
+        {/* profile and logo section end */}
+        {/* search adn card section start */}
+        <div className="flex justify-center items-center h-full w-full gap-10">
+          {/* search section start */}
+          <label className="input input-bordered flex items-center gap-2 w-[30%] text-white inputBg shadow-[#2D3639] shadow-2xl rounded-xl">
+            <FaSearch></FaSearch>
+            <input type="text" className="grow w-full text-white" placeholder="search" />
+          </label>
+          {/* search section end */}
+          {/* profile section start */}
+          <div className="avatar online">
+          <FaShoppingCart className="text-gray-200 text-2xl"></FaShoppingCart>
+          </div>
+          {/* profile section end */}
+        </div>
+        {/* search adn card section end */}
+      </div>
+      {/* content section end */}
+    </div>
+  );
+};
+
+export default Navbar;
