f17d749a622f86d7f9f0f6f040bbbdaa319f1109
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-03 14:47:18 +0600

    creating mobile card

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa6";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
 const Cart = () => {
 
+  // quantity
   const [quantity, setQuantity] = useState(1);
+
+  //toggle button
+  const [open, setOpen] = useState(false)
 
   const datas = [
     {
@@ -134,8 +139,8 @@ const Cart = () => {
   </table>
           </div>
           {/* item table section end */}
-          {/* card section start */}
-          <div className="bg-gray-500 w-full md:w-[30%] space-y-3 p-5 md:p-3 lg:p-5 rounded-3xl h-1/2">
+          {/* desktop card section start */}
+          <div className="bg-gray-500 hidden md:block md:w-[30%] space-y-3 p-5 md:p-3 lg:p-5 rounded-3xl h-1/2">
             <ul>
               <li className="flex justify-between items-center text-sm xl:text-lg "><span className="text-white">Selected Items</span><span className="text-[#00C2C3]">$235.00</span></li>
               <li className="flex justify-between items-center text-sm xl:text-lg "><span className="text-white">Shipping Fee</span><span className="text-[#00C2C3]">$235.00</span></li>
@@ -145,10 +150,32 @@ const Cart = () => {
             <p className="flex justify-between items-center text-sm xl:text-xl font-bold"><span className="text-slate-200">Selected Items</span><span className="text-[#00C2C3]">$235.00</span></p>
             <button className="btn w-full bg-[#00C2C3] hover:bg-[#00C2C3] border-0 outline-none text-white rounded-3xl">Checkout</button>
           </div>
-          {/* card section end */}
+          {/* desktop card section end */}
         </div>
         {/* main section end */}
       </div>
+      {/* mobile card section start */}
+      <div className={`md:hidden w-full bg-white top-0 ${open ? 'right-0 p-9': '-right-96 p-8'} bottom-0 flex items-center z-30 fixed  duration-300`}>
+        <div className="absolute z-10 top-[50%] -ml-11 bg-gray-400 rounded-full">
+          {
+            open ? <FaAngleRight onClick={() => setOpen(!open)} className="text-3xl"></FaAngleRight>:<FaAngleLeft onClick={() => setOpen(!open)} className="text-3xl"></FaAngleLeft>
+          }
+          
+          
+        </div>
+        <div className="bg-gray-500 w-full space-y-3 p-5 md:p-3 lg:p-5 rounded-3xl h-1/2">
+        <ul>
+                <li className="flex justify-between items-center text-sm xl:text-lg "><span className="text-white">Selected Items</span><span className="text-[#00C2C3]">$235.00</span></li>
+                <li className="flex justify-between items-center text-sm xl:text-lg "><span className="text-white">Shipping Fee</span><span className="text-[#00C2C3]">$235.00</span></li>
+                
+              </ul>
+              <div className="divider"></div>
+              <p className="flex justify-between items-center text-sm xl:text-xl font-bold"><span className="text-slate-200">Selected Items</span><span className="text-[#00C2C3]">$235.00</span></p>
+              <button className="btn w-full bg-[#00C2C3] hover:bg-[#00C2C3] border-0 outline-none text-white rounded-3xl">Checkout</button>
+        </div>
+      </div>
+      
+      {/* mobile card section end */}
       {/* content section end */}
     </div>
   );
